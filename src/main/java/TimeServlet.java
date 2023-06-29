3e810ac9224561366522c1e0c1acc67e93467235
BobrovskaOlena <lena69114@gmail.com>
2023-06-29 17:38:35 +0300

    Initial commit

---
+++ src/main/java/TimeServlet.java
@@ -0,0 +1,85 @@
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templateresolver.FileTemplateResolver;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Optional;
+
+
+@WebServlet(value = "/time")
+public class TimeServlet extends HttpServlet {
+    private TemplateEngine templateEngine;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        templateEngine = new TemplateEngine();
+
+        FileTemplateResolver templateResolver = new FileTemplateResolver();
+        templateResolver.setPrefix("src/main/webapp/WEB-INF/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("HTML5");
+        templateResolver.setOrder(templateEngine.getTemplateResolvers().size());
+        templateResolver.setCacheable(false);
+        templateEngine.addTemplateResolver(templateResolver);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws  IOException, ServletException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        Context context = new Context();
+        response.setHeader("Refresh", "1");
+
+        String timeZoneParam = request.getParameter("timezone");
+        ZoneId zoneId = parseTimeZone(timeZoneParam).orElse(ZoneId.of("UTC"));
+
+        ZonedDateTime currentTime = ZonedDateTime.now(zoneId);
+        ZoneOffset currentOffset = currentTime.getOffset();
+        String formattedTime = currentTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z"));
+
+        context.setVariable("formattedTime", formattedTime);
+        context.setVariable("currentOffset", formatOffset(currentOffset));
+        context.setVariable("currentZone", zoneId.toString());
+
+        String output = templateEngine.process("time_template", context);
+        out.write(output);
+        out.close();
+    }
+
+    private Optional<ZoneId> parseTimeZone(String timeZoneParam) {
+        if (timeZoneParam == null || timeZoneParam.isEmpty()) {
+            return Optional.empty();
+        }
+
+        try {
+            return Optional.of(ZoneId.of(timeZoneParam));
+        } catch (Exception e) {
+            try {
+                int offsetHours = Integer.parseInt(timeZoneParam.substring(4));
+                return Optional.of(ZoneOffset.ofHours(offsetHours).normalized());
+            } catch (Exception ex) {
+                return Optional.empty();
+            }
+        }
+    }
+
+    private String formatOffset(ZoneOffset offset) {
+        int totalSeconds = offset.getTotalSeconds();
+        int hours = totalSeconds / 3600;
+        int minutes = Math.abs(totalSeconds % 3600) / 60;
+
+        return String.format("%+03d:%02d", hours, minutes);
+    }
+}
+
---
+++ src/main/java/TimeZoneValidateFilter.java
@@ -0,0 +1,58 @@
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.TimeZone;
+
+@WebFilter("/time")
+public class TimeZoneValidateFilter extends HttpFilter {
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse resp, FilterChain chain)
+            throws IOException, ServletException {
+        String timeZoneParam = req.getParameter("timezone");
+
+        if (timeZoneParam == null || timeZoneParam.isEmpty()) {
+            timeZoneParam = TimeZone.getDefault().getID();
+        } else if (timeZoneParam.equals("UTC")) {
+            timeZoneParam = TimeZone.getDefault().getID();
+        } else if (!isValidTimeZone(timeZoneParam)) {
+            resp.setStatus(400);
+            resp.getWriter().write("Invalid timezone");
+            return;
+        }
+
+        req.setAttribute("timezone", timeZoneParam);
+        chain.doFilter(req, resp);
+    }
+
+    private boolean isValidTimeZone(String timeZoneParam) {
+        if (timeZoneParam.length() < 4 || !timeZoneParam.startsWith("UTC")) {
+            return false;
+        }
+
+        char sign = timeZoneParam.charAt(3);
+        String offset = timeZoneParam.substring(4);
+
+        if (sign == '+' || sign == '-') {
+            try {
+                int offsetHours = Integer.parseInt(offset);
+                return offsetHours >= 0 && offsetHours <= 23;
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+
+
+
+
+
+
+
